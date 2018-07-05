966e1a86da2cb4bc6b943a94bb9e422553fb8e98
Benjamin Benno Falkner <benno.falkner@gmail.com>
2018-07-06 00:32:23 +0200

    recall removed old code

--- js/recall.js
+++ js/recall.js
@@ -140,58 +140,7 @@ var ReCall = (function() {
         }
     }
 
-    var Autocomplete = function (name, url, urlupdate){
-        this.name = name;
-        this.Search = $(name);
-        this.AC = null;
-        this.url = url;
-        this.urlupdate = urlupdate;
-        this.data = AutocompleteDefaultData;
     
-        this.Search.keyup(this.update.bind(this));
-        $(document).click(this.delete.bind(this));
-    }
-    
-    var AutocompleteDefaultData = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-    Autocomplete.prototype.update = function(e){
-        // request data
-        var val = this.Search.val();
-        console.log('update: '+ val)
-        //skip
-        
-        if (val.length > 0) {
-            //search 
-    
-            // check ich autocomplete exsits
-            if(this.AC == null ) {
-                this.Search.parent().append("<div id=\""+this.name.substr(1)+"autocomplete\" class=\"autocomplete\"></div>");
-                this.AC = $(this.name+"autocomplete");
-            }
-    
-            // loop data
-            var inner = "<ul>";
-            var count = 0;
-            for (i = 0; i < this.data.length; i++) {
-                if (this.data[i].substr(0, val.length).toLowerCase() == val.toLowerCase()) {
-                    inner += "<li><a href=\""+this.url+this.data[i]+"\"><strong>" + this.data[i].substr(0, val.length) + "</strong>"+this.data[i].substr(val.length)+"</li>";
-                    count++;
-                }
-            }
-            if(count>0) {
-                inner += "</ul>";
-                this.AC.html(inner);
-                return 
-            }
-        }
-        this.delete();
-    }
-    
-    Autocomplete.prototype.delete = function(e){
-        if(this.AC!=null){
-            this.AC.remove();
-            this.AC = null;
-        }
-    }
  
 
     // create a Table handler
